6de323de8c189bd5441fc5d52742a8a58763ed2c
aishwaryaadaragunchi26-code <aishwaryaadaragunchi26@gmail.com>
2026-08-28 11:35:19 +0530

    Update names.py

--- names.py
+++ names.py
@@ -1,4 +1,6 @@
 name = input("enter your name")
 surname = input("Enter your surname")
+contact = input("Enter your contact details")
 
 print("your details is: ", name+" "+surname)
+print("your contact is: ", contact)
